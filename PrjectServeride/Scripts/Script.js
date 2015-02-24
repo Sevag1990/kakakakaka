8f88731aaa7737553b45dba2f524eb0c6e1d87eb
nipple1450 <kevin.ljung@outlook.com>
2015-02-24 16:35:41 +0100

    sadasdas

---
+++ PrjectServeride/Scripts/Script.js
@@ -0,0 +1,20 @@
+﻿function loadAll()
+{
+  
+    document.getElementById("ActiveS").innerHTML = "All"
+
+   
+
+}
+function loadAuthor() {
+    document.getElementById("ActiveS").innerHTML = "Author";
+   
+
+}
+function loadTitle() {
+    document.getElementById("ActiveS").innerHTML = "Title";
+
+}
+function loadRegForm() {
+    document.getElementById("registerFrm").style.visibility = "visible";
+}
